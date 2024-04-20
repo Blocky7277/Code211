fb4aca82c6365ba8bbdbfd2d5f45c4b6098eff06
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2024-04-20 13:40:30 -0500

    Update AnimatePowerUp.cs

--- Assets/AnimatePowerUp.cs
+++ Assets/AnimatePowerUp.cs
@@ -11,15 +11,46 @@ public class AnimatePowerUp : MonoBehaviour
     public Sprite sprite5;
     public Sprite sprite6;
     public Sprite sprite7;
+    private SpriteRenderer spriteRenderer;
+    private float frameCount = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Time.frameCount % 20 == 0){
+            if (frameCount == 1){
+                spriteRenderer.sprite = sprite1;
+                frameCount++;
+            }
+            else if (frameCount == 2){
+                spriteRenderer.sprite = sprite2;
+                frameCount++;
+            }
+            else if (frameCount == 3){
+                spriteRenderer.sprite = sprite3;
+                frameCount++;
+            }
+            else if (frameCount == 4){
+                spriteRenderer.sprite = sprite4;
+                frameCount++;
+            }
+            else if (frameCount == 5){
+                spriteRenderer.sprite = sprite5;
+                frameCount++;
+            }
+            else if (frameCount == 6){
+                spriteRenderer.sprite = sprite6;
+                frameCount++;
+            }
+            else if (frameCount == 7){
+                spriteRenderer.sprite = sprite7;
+                frameCount = 1;
+            }
+        }
     }
 }
