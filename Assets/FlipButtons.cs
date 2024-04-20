c31500b53742e52e4a19f0152b8829654a1dd6b0
Anirudh R <anirudhrajan07@gmail.com>
2024-04-20 13:10:44 -0500

    Fixes

--- Assets/FlipButtons.cs
+++ Assets/FlipButtons.cs
@@ -9,13 +9,15 @@ public class FlipButtons : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+
     }
 
-    void OnCollisionStay2D(Collision2D obj) {
-        if (obj.gameObject.tag == "Player") {            
-            obj.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            obj.GetComponent<Rigidbody2D>().gravityScale *= -1;
+    void OnCollisionStay2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Player")
+        {
+            obj.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            obj.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
         }
     }
 }
