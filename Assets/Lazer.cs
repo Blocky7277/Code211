d0a0e90c26c8ffd2d4c26ea9ed25d19b887849d4
Blocky7277 <jacob612r@gmail.com>
2024-04-20 18:55:44 -0500

    Lazer

---
+++ Assets/Lazer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class Lazer : MonoBehaviour
+{
+    public int interval = 3;
+
+    public int variation = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+
+    IEnumerator Timer() {
+        GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+        yield return new WaitForSeconds(Random.Range(interval-variation, interval+variation));
+        StartCoroutine(Timer());
+    }
+
+    void OnTriggerEnter2D(Collider2D obj) {
+        if(GetComponent<Renderer>().enabled) obj.gameObject.GetComponent<Movement>().defeatPlayer();
+    }
+}
