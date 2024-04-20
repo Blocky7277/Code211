552e8a51dcb35a892bedfb100f4475085c1d35c6
Joshua <joshua.lin.1281@gmail.com>
2024-04-20 13:00:11 -0500

    Update FlipButtons.cs

--- Assets/FlipButtons.cs
+++ Assets/FlipButtons.cs
@@ -6,16 +6,21 @@ using UnityEngine.SceneManagement;
 public class FlipButtons : MonoBehaviour
 {
     public Movement playerMovement;
+    // Ticks left until the player can start moving
+    public int ticksLeft = -1;
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (ticksLeft > -1) {
+            ticksLeft -= 1;
+        }
     }
 
     void OnCollisionStay2D(Collision2D obj) {
         if (obj.gameObject.tag == "Player") {            
-            obj.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            obj.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            if (ticksLeft == -1) {
+                obj.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            }            
         }
     }
 }
