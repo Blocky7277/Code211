7a456472525c85160056c282eb759817fffc4a9e
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2024-04-20 13:17:11 -0500

    started powerup

---
+++ Assets/AnimatePowerUp.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatePowerUp : MonoBehaviour
+{
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
+    public Sprite sprite4;
+    public Sprite sprite5;
+    public Sprite sprite6;
+    public Sprite sprite7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
